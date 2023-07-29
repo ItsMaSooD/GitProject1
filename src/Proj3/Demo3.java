36e01de2a6956dfb666a1d0f9293c4ed6e91307f
masoo <masoo@192.168.1.170>
2023-07-28 20:31:55 -0500

    First Commit

---
+++ src/Proj3/Demo3.java
@@ -0,0 +1,26 @@
+package Proj3;
+
+public class Demo3 {
+	
+public static void main(String args []) {
+		
+		//Step 3 ( first write isEqual() then come back here add
+		
+		isEqual (5, 5);
+		isEqual(255, -888);
+		isEqual(765, 123);
+		isEqual(22, 999);
+		
+	}
+
+	//write a method that take 2 int parameters and it checks
+	//if they are equal or not
+	public static void isEqual(int num1, int num2) {
+		
+		if (num1 == num2) {
+			System.out.println("EQUALS");
+			} else {
+				System.out.println("NOT EQUALS");
+		}
+	}
+}
